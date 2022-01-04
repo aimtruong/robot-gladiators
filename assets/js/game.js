221ce3d1f1eb07232834f116dc7eea5e4a2d0d47
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-04 00:05:20 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,15 +5,13 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-// enemy-robot
-var enemyName = "Roborto";
+// enemy-robots
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // function "fight"
-var fight = function(){
+var fight = function(enemyName){
     // Alert players that they are starting the round
         window.alert("Welcome to Robot Gladiators!");
 
@@ -80,4 +78,7 @@ var fight = function(){
         }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
+
