134793bf5e0b3da8fe67686f8e65b31b456fbb86
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-05 23:47:13 -0800

    create random function to see who attacks first

--- assets/js/game.js
+++ assets/js/game.js
@@ -34,42 +34,68 @@ var fightOrSkip = function(){
 
 // function "fight" for player-robot to fight enemy-robot
 var fight = function(enemy){
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if(Math.random() > 0.5){
+        isPlayerTurn = false;
+    }
+
     while(playerInfo.health > 0 && enemy.health > 0){
-        
-        // if player choose to skip, then skip
-        if (fightOrSkip()){
-            // confirm player wants to skip
-            break;
-        }
-        
-        // generate random damage value based on player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage);
-        // Log a resulting message to the console so we know that it worked.
-        console.log(
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+        // if player attacks first
+        if(isPlayerTurn){
+            // if player choose to skip, then skip
+            if (fightOrSkip()){
+                // confirm player wants to skip
+                break;
+            }
+
+            // generate random damage value based on player's attack power
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            enemy.health = Math.max(0, enemy.health - damage);
+            // Log a resulting message to the console so we know that it worked.
+            console.log(
+                playerInfo.name + 
+                " attacked " + 
+                enemy.name + 
+                ". " + 
+                enemy.name + 
+                " now has " + 
+                enemy.health + 
+                " health remaining."
             );
-            
+                
             // Check enemy's health
             if (enemy.health <= 0){
                 window.alert(enemy.name + " has died!");
-                
+                    
                 // award player money for winning
                 playerInfo.money = playerInfo.money + 20;
                 break;
             }
             else{
-                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+                window.alert(enemy.name + 
+                " still has " + 
+                enemy.health + 
+                " health left.");
             }
-            
-            // generate random damage value based on enemy's attack
+        }
+        // if enemy-robot attacks first
+        else{
             var damage = randomNumber(enemy.attack - 3, enemy.attack);
+            
             playerInfo.health = Math.max(0, playerInfo.health - damage);
             // Log a resulting message to the console so we know that it worked.
             console.log(
-                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+                enemy.name + 
+                " attacked " + 
+                playerInfo.name + 
+                ". " + 
+                playerInfo.name + 
+                " now has " + 
+                playerInfo.health + 
+                " health remaining."
                 );
-                
+                    
                 // check player's health
                 if (playerInfo.health <= 0){
                     window.alert(playerInfo.name + " has died!");
@@ -78,6 +104,10 @@ var fight = function(enemy){
                 else{
                     window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
                 }
+        }
+    
+        // switch turn order for next round
+    isPlayerTurn = !isPlayerTurn;
     }
 };
 
