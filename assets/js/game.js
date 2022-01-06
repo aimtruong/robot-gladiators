e34374a0b823df0b9ae43737e72ed7085466efd5
Aimee Truong <aimtruong@ucdavis.edu>
2022-01-06 00:07:43 -0800

    add highscore feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,12 +151,31 @@ var startGame = function(){
 
 // function to end the entire game
 var endGame = function(){
-    // player wins
-    if(playerInfo.health > 0){
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score
+    var highScore = localStorage.getItem("highscore");
+    if(highScore === null){
+        highScore = 0;
+    }
+
+    // if player has more money than highscore
+    if(playerInfo.money > highScore){
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name +
+            " now has the high score of " +
+            playerInfo.money + 
+            "!"
+            );       
     }
     else{
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name +
+            " did not beat the high score of " +
+            highScore +
+            ". Maybe next time!"
+            );
     }
 
     // ask player to play again
